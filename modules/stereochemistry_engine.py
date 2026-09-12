0f4fb6afc658ab5d418512dd064c1e14f49a8b46
bhavinochem-gif <bhavinochem@gmail.com>
2026-09-12 23:11:36 +0530

    Delete modules directory

--- modules/stereochemistry_engine.py
+++
@@ -1,5 +0,0 @@
-from rdkit import Chem
-def analyze_stereochemistry(rs,ps):
- def f(s):
-  m=Chem.MolFromSmiles(s or "");return{"valid":bool(m),"centers":[{"atom":a,"label":b} for a,b in Chem.FindMolChiralCenters(m,includeUnassigned=True,includeCIP=True)]} if m else{"valid":False,"centers":[]}
- return{"reactant":f(rs),"product":f(ps)}
